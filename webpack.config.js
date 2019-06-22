54506f29cb14754385078653f4fcdfb585980c42
Javier Eguiluz <javier.eguiluz@gmail.com>
2019-06-22 17:18:26 +0200

    Removed the bootstrap-all files

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,6 @@ Encore
 
     .addEntry('app', './assets/js/app.js')
     .addEntry('app-rtl', './assets/js/app-rtl.js')
-    .addEntry('bootstrap-all', './assets/js/bootstrap-all.js')
 ;
 
 module.exports = Encore.getWebpackConfig();
